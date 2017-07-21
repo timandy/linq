429e9d588a0f198083c498a17e6076940c9b7b2f
TimAndy <xuchonglei@126.com>
2017-07-21 17:41:06 +0800

    格式化代码

--- src/main/java/com/bestvike/linq/IEqualityComparer.java
+++ src/main/java/com/bestvike/linq/IEqualityComparer.java
@@ -1,14 +1,10 @@
 package com.bestvike.linq;
 
-import java.util.Objects;
-
 /**
  * @author 许崇雷
  * @date 2017/7/12
  */
 public interface IEqualityComparer<T> {
-
-
     boolean equals(T x, T y);
 
     int getHashCode(T obj);
