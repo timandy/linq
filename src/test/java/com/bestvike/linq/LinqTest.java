1415920ebc48045e0cd5a955b8c4caef1e86942c
TimAndy <xuchonglei@126.com>
2017-07-24 17:10:29 +0800

    添加 Linq 类单元测试

---
+++ src/test/java/com/bestvike/linq/LinqTest.java
@@ -0,0 +1,216 @@
+package com.bestvike.linq;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * @author 许崇雷
+ * @date 2017/7/24
+ */
+public class LinqTest {
+    @Test
+    public void testBoolean() {
+        final boolean[] array = {true, false, true};
+        final List<Boolean> list = new ArrayList<>();
+        list.add(true);
+        list.add(false);
+        list.add(true);
+        Assert.assertEquals(3, Linq.asEnumerable(array).count());
+        Assert.assertEquals(3, Linq.asEnumerable(list).count());
+        Assert.assertTrue(Linq.asEnumerable(array).sequenceEqual(Linq.asEnumerable(list)));
+    }
+
+    @Test
+    public void testByte() {
+        final byte[] array = {0x01, 0x02, 0x03};
+        final List<Byte> list = new ArrayList<>();
+        list.add(Byte.valueOf("01"));
+        list.add(Byte.valueOf("02"));
+        list.add(Byte.valueOf("03"));
+        Assert.assertEquals(3, Linq.asEnumerable(array).count());
+        Assert.assertEquals(3, Linq.asEnumerable(list).count());
+        Assert.assertTrue(Linq.asEnumerable(array).sequenceEqual(Linq.asEnumerable(list)));
+    }
+
+    @Test
+    public void testShort() {
+        final short[] array = {0x01, 0x02, 0x03};
+        final List<Short> list = new ArrayList<>();
+        list.add(Short.valueOf("01"));
+        list.add(Short.valueOf("02"));
+        list.add(Short.valueOf("03"));
+        Assert.assertEquals(3, Linq.asEnumerable(array).count());
+        Assert.assertEquals(3, Linq.asEnumerable(list).count());
+        Assert.assertTrue(Linq.asEnumerable(array).sequenceEqual(Linq.asEnumerable(list)));
+    }
+
+    @Test
+    public void testInt() {
+        final int[] array = {0x01, 0x02, 0x03};
+        final List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        Assert.assertEquals(3, Linq.asEnumerable(array).count());
+        Assert.assertEquals(3, Linq.asEnumerable(list).count());
+        Assert.assertTrue(Linq.asEnumerable(array).sequenceEqual(Linq.asEnumerable(list)));
+    }
+
+    @Test
+    public void testLong() {
+        final long[] array = {0x01, 0x02, 0x03};
+        final List<Long> list = new ArrayList<>();
+        list.add(1L);
+        list.add(2L);
+        list.add(3L);
+        Assert.assertEquals(3, Linq.asEnumerable(array).count());
+        Assert.assertEquals(3, Linq.asEnumerable(list).count());
+        Assert.assertTrue(Linq.asEnumerable(array).sequenceEqual(Linq.asEnumerable(list)));
+    }
+
+    @Test
+    public void testFloat() {
+        final float[] array = {1f, 2f, Float.NaN};
+        final List<Float> list = new ArrayList<>();
+        list.add(1f);
+        list.add(2f);
+        list.add(Float.NaN);
+        Assert.assertEquals(true, Objects.equals(Float.NaN, Float.NaN));
+        Assert.assertEquals(3, Linq.asEnumerable(array).count());
+        Assert.assertEquals(3, Linq.asEnumerable(list).count());
+        Assert.assertTrue(Linq.asEnumerable(array).sequenceEqual(Linq.asEnumerable(list)));
+    }
+
+    @Test
+    public void testDouble() {
+        final double[] array = {1d, 2d, Double.NaN};
+        final List<Double> list = new ArrayList<>();
+        list.add(1d);
+        list.add(2d);
+        list.add(Double.NaN);
+        Assert.assertEquals(true, Objects.equals(Double.NaN, Double.NaN));
+        Assert.assertEquals(3, Linq.asEnumerable(array).count());
+        Assert.assertEquals(3, Linq.asEnumerable(list).count());
+        Assert.assertTrue(Linq.asEnumerable(array).sequenceEqual(Linq.asEnumerable(list)));
+    }
+
+    @Test
+    public void testChar() {
+        final char[] array = {'a', 'b', 'c'};
+        final List<Character> list = new ArrayList<>();
+        list.add('a');
+        list.add('b');
+        list.add('c');
+        Assert.assertEquals(3, Linq.asEnumerable(array).count());
+        Assert.assertEquals(3, Linq.asEnumerable(list).count());
+        Assert.assertTrue(Linq.asEnumerable(array).sequenceEqual(Linq.asEnumerable(list)));
+    }
+
+    @Test
+    public void testArrayAndList() {
+        final String[] array = {"1", "2", "3"};
+        final List<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        Assert.assertEquals(3, Linq.asEnumerable(array).count());
+        Assert.assertEquals(3, Linq.asEnumerable(list).count());
+        Assert.assertTrue(Linq.asEnumerable(array).sequenceEqual(Linq.asEnumerable(list)));
+    }
+
+    @Test
+    public void testCollectionAndIterable() {
+        final Set<String> set = new HashSet<>();
+        set.add("1");
+        set.add("2");
+        set.add("3");
+        final Iterable<String> iterableDemo = new IterableDemo();
+        Assert.assertEquals(3, Linq.asEnumerable(set).count());
+        Assert.assertEquals(3, Linq.asEnumerable(iterableDemo).count());
+        Assert.assertTrue(Linq.asEnumerable(set).sequenceEqual(Linq.asEnumerable(iterableDemo)));
+    }
+
+    @Test
+    public void testMap() {
+        final Map<Integer, String> map = new HashMap<>();
+        map.put(1, "1");
+        map.put(2, "2");
+        map.put(3, "3");
+        IEnumerable<Map.Entry<Integer, String>> enu1 = Linq.asEnumerable(map);
+
+        final Map<Integer, String> map2 = new HashMap<>();
+        map2.put(1, "1");
+        map2.put(2, "2");
+        map2.put(3, "3");
+        IEnumerable<Map.Entry<Integer, String>> enu2 = Linq.asEnumerable(map2);
+        map2.put(4, "4");
+        map2.remove(4);
+
+        Assert.assertEquals(3, Linq.asEnumerable(enu1).count());
+        Assert.assertEquals(3, Linq.asEnumerable(enu2).count());
+    }
+
+    @Test
+    public void testSingleton() {
+        final String[] array = {"1"};
+        Assert.assertEquals(1, Linq.asEnumerable(array).count());
+        Assert.assertEquals(1, Linq.singletonEnumerable("1").count());
+        Assert.assertTrue(Linq.asEnumerable(array).sequenceEqual(Linq.singletonEnumerable("1")));
+    }
+
+    @Test
+    public void testRange() {
+        final Integer[] array = {1, 2, 3, 4, 5};
+        final IEnumerable<Integer> integers = Linq.range(1, 5);
+        Assert.assertEquals(5, integers.count());
+        Assert.assertTrue(Linq.asEnumerable(array).sequenceEqual(integers));
+    }
+
+    @Test
+    public void testRepeat() {
+        final Integer[] array = {1, 1, 1, 1, 1};
+        final IEnumerable<Integer> integers = Linq.repeat(1, 5);
+        Assert.assertEquals(5, integers.count());
+        Assert.assertTrue(Linq.asEnumerable(array).sequenceEqual(integers));
+    }
+
+    @Test
+    public void testEmpty() {
+        final Integer[] array = {};
+        final IEnumerable<Integer> integers = Linq.empty();
+        Assert.assertEquals(0, integers.count());
+        Assert.assertTrue(Linq.asEnumerable(array).sequenceEqual(integers));
+    }
+
+    static final class IterableDemo implements Iterable<String> {
+        @Override
+        public Iterator<String> iterator() {
+            return new IteratorDemo();
+        }
+    }
+
+    static final class IteratorDemo implements Iterator<String> {
+        int current = 0;
+
+
+        @Override
+        public boolean hasNext() {
+            return this.current < 3;
+        }
+
+        @Override
+        public String next() {
+            this.current++;
+            return String.valueOf(this.current);
+        }
+    }
+}
