14634c4fb811611c0e21de9256302a713d96d5ee
TimAndy <xuchonglei@126.com>
2017-07-26 23:04:06 +0800

    修复 Buffer 转数组时没有取实际大小的 Bug

--- src/main/java/com/bestvike/linq/impl/Buffer.java
+++ src/main/java/com/bestvike/linq/impl/Buffer.java
@@ -3,6 +3,7 @@ package com.bestvike.linq.impl;
 import com.bestvike.linq.ICollectionEnumerable;
 import com.bestvike.linq.IEnumerable;
 import com.bestvike.linq.util.Array;
+import com.bestvike.linq.util.ArrayUtils;
 
 /**
  * @author 许崇雷
@@ -43,6 +44,8 @@ public final class Buffer<TElement> {
     }
 
     public TElement[] toArray(Class<TElement> clazz) {
-        return this.items.toArray(clazz);
+        TElement[] array = ArrayUtils.newInstance(clazz, this.count);
+        Array.copy(this.items, 0, array, 0, this.count);
+        return array;
     }
 }
