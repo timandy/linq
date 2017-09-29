af33d2cb02848eed4f54672a3310496d44182092
TimAndy <xuchonglei@126.com>
2017-09-29 10:55:29 +0800

    字段重命名

--- src/main/java/com/bestvike/collections/generic/ArrayBuilder.java
+++ src/main/java/com/bestvike/collections/generic/ArrayBuilder.java
@@ -9,8 +9,8 @@ public final class ArrayBuilder<T> {
     private static final int DefaultCapacity = 4;
     private static final int MaxCoreClrArrayLength = 0x7fefffff; // For byte arrays the limit is slightly larger
 
-    private Array<T> _array;    // Starts out null, initialized on first Add.
-    private int _count;         // Number of items into _array we're using.
+    private Array<T> array;     // Starts out null, initialized on first Add.
+    private int count;          // Number of items into array we're using.
 
     public ArrayBuilder() {
     }
@@ -18,88 +18,88 @@ public final class ArrayBuilder<T> {
     public ArrayBuilder(int capacity) {
         assert capacity >= 0;
         if (capacity > 0)
-            this._array = Array.create(capacity);
+            this.array = Array.create(capacity);
     }
 
     public int capacity() {
-        return this._array == null ? 0 : this._array.length();
+        return this.array == null ? 0 : this.array.length();
     }
 
     public int count() {
-        return this._count;
+        return this.count;
     }
 
     public T get(int index) {
-        assert index >= 0 && index < this._count;
-        return this._array.get(index);
+        assert index >= 0 && index < this.count;
+        return this.array.get(index);
     }
 
     public void set(int index, T value) {
-        assert index >= 0 && index < this._count;
-        this._array.set(index, value);
+        assert index >= 0 && index < this.count;
+        this.array.set(index, value);
     }
 
     public void add(T item) {
-        if (this._count == this.capacity())
-            this.ensureCapacity(this._count + 1);
+        if (this.count == this.capacity())
+            this.ensureCapacity(this.count + 1);
         this.uncheckedAdd(item);
     }
 
     public T first() {
-        assert this._count > 0;
-        return this._array.get(0);
+        assert this.count > 0;
+        return this.array.get(0);
     }
 
     public T last() {
-        assert this._count > 0;
-        return this._array.get(this._count - 1);
+        assert this.count > 0;
+        return this.array.get(this.count - 1);
     }
 
     public T[] toArray(Class<T> clazz) {
-        if (this._count == 0)
+        if (this.count == 0)
             return ArrayUtils.empty(clazz);
 
-        assert this._array != null; // Nonzero _count should imply this
+        assert this.array != null; // Nonzero count should imply this
         T[] result;
-        if (this._count < this._array.length()) {
+        if (this.count < this.array.length()) {
             // Avoid a bit of overhead (method call, some branches, extra codegen)
             // which would be incurred by using Array.Resize
-            result = ArrayUtils.newInstance(clazz, this._count);
-            Array.copy(this._array, 0, result, 0, this._count);
+            result = ArrayUtils.newInstance(clazz, this.count);
+            Array.copy(this.array, 0, result, 0, this.count);
         } else {
-            result = this._array.toArray(clazz);
+            result = this.array.toArray(clazz);
         }
 //#if DEBUG
-//        // Try to prevent callers from using the ArrayBuilder after toArray, if _count != 0.
-//        _count = -1;
-//        _array = null;
+//        // Try to prevent callers from using the ArrayBuilder after toArray, if count != 0.
+//        count = -1;
+//        array = null;
 //#endif
         return result;
     }
 
     public Array<T> toArray() {
-        if (this._count == 0)
+        if (this.count == 0)
             return Array.empty();
 
-        assert this._array != null; // Nonzero _count should imply this
-        Array<T> result = this._array;
-        if (this._count < this._array.length()) {
+        assert this.array != null; // Nonzero count should imply this
+        Array<T> result = this.array;
+        if (this.count < this.array.length()) {
             // Avoid a bit of overhead (method call, some branches, extra codegen)
             // which would be incurred by using Array.Resize
-            result = Array.create(this._count);
-            Array.copy(this._array, 0, result, 0, this._count);
+            result = Array.create(this.count);
+            Array.copy(this.array, 0, result, 0, this.count);
         }
 //#if DEBUG
-//        // Try to prevent callers from using the ArrayBuilder after toArray, if _count != 0.
-//        _count = -1;
-//        _array = null;
+//        // Try to prevent callers from using the ArrayBuilder after toArray, if count != 0.
+//        count = -1;
+//        array = null;
 //#endif
         return result;
     }
 
     public void uncheckedAdd(T item) {
-        assert this._count < this.capacity();
-        this._array.set(this._count++, item);
+        assert this.count < this.capacity();
+        this.array.set(this.count++, item);
     }
 
     private void ensureCapacity(int minimum) {
@@ -110,8 +110,8 @@ public final class ArrayBuilder<T> {
             nextCapacity = Math.max(capacity + 1, MaxCoreClrArrayLength);
         nextCapacity = Math.max(nextCapacity, minimum);
         Array<T> next = Array.create(nextCapacity);
-        if (this._count > 0)
-            Array.copy(this._array, 0, next, 0, this._count);
-        this._array = next;
+        if (this.count > 0)
+            Array.copy(this.array, 0, next, 0, this.count);
+        this.array = next;
     }
 }
