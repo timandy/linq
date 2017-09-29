936c336d50e27d21a496b3ed534f68ed3104356c
TimAndy <xuchonglei@126.com>
2017-09-29 13:36:48 +0800

    实现 SingleLinkedNode

---
+++ src/main/java/com/bestvike/collections/generic/SingleLinkedNode.java
@@ -0,0 +1,85 @@
+package com.bestvike.collections.generic;
+
+import com.bestvike.linq.IEnumerator;
+import com.bestvike.linq.enumerator.ArrayEnumerator;
+import com.bestvike.linq.util.ArrayUtils;
+
+/**
+ * Created by 许崇雷 on 2017-09-11.
+ */
+public final class SingleLinkedNode<TSource> {
+    private SingleLinkedNode<TSource> linked;
+    private TSource item;
+
+    public SingleLinkedNode(TSource item) {
+        this.item = item;
+    }
+
+    private SingleLinkedNode(SingleLinkedNode<TSource> linked, TSource item) {
+        assert linked != null;
+        this.linked = linked;
+        this.item = item;
+    }
+
+    public TSource getItem() {
+        return this.item;
+    }
+
+    public SingleLinkedNode<TSource> getLinked() {
+        return this.linked;
+    }
+
+    public SingleLinkedNode<TSource> add(TSource item) {
+        return new SingleLinkedNode<>(this, item);
+    }
+
+    public int getCount() {
+        int count = 0;
+        for (SingleLinkedNode<TSource> node = this; node != null; node = node.linked)
+            count++;
+        return count;
+    }
+
+    public IEnumerator<TSource> getEnumerator(int count) {
+        return new ArrayEnumerator<>(this.toArray(count));
+    }
+
+    public SingleLinkedNode<TSource> getNode(int index) {
+        assert index >= 0 && index < this.getCount();
+
+        SingleLinkedNode<TSource> node = this;
+        for (; index > 0; index--)
+            node = node.linked;
+
+        assert node != null;
+        return node;
+    }
+
+    private TSource[] toArray(Class<TSource> clazz, int count) {
+        assert count == this.getCount();
+
+        TSource[] array = ArrayUtils.newInstance(clazz, count);
+        int index = count;
+        for (SingleLinkedNode<TSource> node = this; node != null; node = node.linked) {
+            --index;
+            array[index] = node.item;
+        }
+
+        assert index == 0;
+        return array;
+    }
+
+    private Array<TSource> toArray(int count) {
+        assert count == this.getCount();
+
+        Array<TSource> array = Array.create(count);
+        int index = count;
+        for (SingleLinkedNode<TSource> node = this; node != null; node = node.linked) {
+            --index;
+            array.set(index, node.item);
+        }
+
+        assert index == 0;
+        return array;
+    }
+}
