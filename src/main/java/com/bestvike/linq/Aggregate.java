3cd99557b477a755e769716a1bd22150ba1ce259
TimAndy <xuchonglei@126.com>
2017-09-29 10:16:32 +0800

    添加 Aggregate

---
+++ src/main/java/com/bestvike/linq/Aggregate.java
@@ -0,0 +1,52 @@
+package com.bestvike.linq;
+
+import com.bestvike.linq.exception.Errors;
+import com.bestvike.linq.function.Func1;
+import com.bestvike.linq.function.Func2;
+
+/**
+ * Created by 许崇雷 on 2017-09-11.
+ */
+final class Aggregate {
+    private Aggregate() {
+    }
+
+    public static <TSource> TSource aggregate(IEnumerable<TSource> source, Func2<TSource, TSource, TSource> func) {
+        if (source == null)
+            throw Errors.argumentNull("source");
+        if (func == null)
+            throw Errors.argumentNull("func");
+        try (IEnumerator<TSource> e = source.enumerator()) {
+            if (!e.moveNext())
+                throw Errors.noElements();
+            TSource result = e.current();
+            while (e.moveNext())
+                result = func.apply(result, e.current());
+            return result;
+        }
+    }
+
+    public static <TSource, TAccumulate> TAccumulate aggregate(IEnumerable<TSource> source, TAccumulate seed, Func2<TAccumulate, TSource, TAccumulate> func) {
+        if (source == null)
+            throw Errors.argumentNull("source");
+        if (func == null)
+            throw Errors.argumentNull("func");
+        TAccumulate result = seed;
+        for (TSource element : source)
+            result = func.apply(result, element);
+        return result;
+    }
+
+    public static <TSource, TAccumulate, TResult> TResult aggregate(IEnumerable<TSource> source, TAccumulate seed, Func2<TAccumulate, TSource, TAccumulate> func, Func1<TAccumulate, TResult> resultSelector) {
+        if (source == null)
+            throw Errors.argumentNull("source");
+        if (func == null)
+            throw Errors.argumentNull("func");
+        if (resultSelector == null)
+            throw Errors.argumentNull("resultSelector");
+        TAccumulate result = seed;
+        for (TSource element : source)
+            result = func.apply(result, element);
+        return resultSelector.apply(result);
+    }
+}
