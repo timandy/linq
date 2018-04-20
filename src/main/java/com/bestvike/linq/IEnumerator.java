96cfd89d8c2f3e82148535e97cc4c34f3d48f59b
TimAndy <xuchonglei@126.com>
2018-04-20 18:07:01 +0800

    添加 IDisposable 接口

--- src/main/java/com/bestvike/linq/IEnumerator.java
+++ src/main/java/com/bestvike/linq/IEnumerator.java
@@ -1,11 +1,13 @@
 package com.bestvike.linq;
 
+import com.bestvike.IDisposable;
+
 import java.util.Iterator;
 
 /**
  * Created by 许崇雷 on 2017/7/10.
  */
-public interface IEnumerator<T> extends Iterator<T>, AutoCloseable {
+public interface IEnumerator<T> extends Iterator<T>, IDisposable {
     boolean moveNext();
 
     T current();
