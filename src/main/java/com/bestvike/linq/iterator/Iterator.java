bcb0c01ff2d3bfa622f16840599c3a6b8cf7f873
TimAndy <xuchonglei@126.com>
2018-04-23 10:44:02 +0800

    提交 Iterator

--- src/main/java/com/bestvike/linq/iterator/Iterator.java
+++ src/main/java/com/bestvike/linq/iterator/Iterator.java
@@ -1,13 +1,18 @@
 package com.bestvike.linq.iterator;
 
+import com.bestvike.function.Func1;
 import com.bestvike.linq.IEnumerable;
-import com.bestvike.linq.function.Func1;
+import com.bestvike.linq.iteratorold.WhereEnumerableIterator;
 
 /**
  * Created by 许崇雷 on 2017/7/10.
  */
-abstract class Iterator<TSource> extends AbstractIterator<TSource> {
-    public abstract <TResult> IEnumerable<TResult> internalSelect(Func1<TSource, TResult> selector);
+public abstract class Iterator<TSource> extends AbstractIterator<TSource> {
+    public <TResult> IEnumerable<TResult> _select(Func1<TSource, TResult> selector) {
+        return new SelectEnumerableIterator<TSource, TResult>(this, selector);
+    }
 
-    public abstract IEnumerable<TSource> internalWhere(Func1<TSource, Boolean> predicate);
+    public IEnumerable<TSource> _where(Func1<TSource, Boolean> predicate) {
+        return new WhereEnumerableIterator<>(this, predicate);
+    }
 }
