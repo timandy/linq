1f0d7ee1eb91eff65a9fc304a99a5ca2f8f110d1
TimAndy <xuchonglei@126.com>
2018-04-27 10:22:15 +0800

    完成 Contains

---
+++ src/main/java/com/bestvike/linq/iterator/Contains.java
@@ -0,0 +1,34 @@
+package com.bestvike.linq.iterator;
+
+import com.bestvike.collections.generic.EqualityComparer;
+import com.bestvike.collections.generic.ICollection;
+import com.bestvike.collections.generic.IEqualityComparer;
+import com.bestvike.linq.IEnumerable;
+import com.bestvike.linq.exception.Errors;
+
+/**
+ * Created by 许崇雷 on 2018-04-27.
+ */
+public final class Contains {
+    public static <TSource> boolean contains(IEnumerable<TSource> source, TSource value) {
+        return source instanceof ICollection
+                ? ((ICollection<TSource>) source)._contains(value)
+                : contains(source, value, null);
+
+    }
+
+    public static <TSource> boolean contains(IEnumerable<TSource> source, TSource value, IEqualityComparer<TSource> comparer) {
+        if (source == null)
+            throw Errors.argumentNull("source");
+
+        if (comparer == null)
+            comparer = EqualityComparer.Default();
+
+        for (TSource element : source) {
+            if (comparer.equals(element, value))
+                return true;
+        }
+
+        return false;
+    }
+}
