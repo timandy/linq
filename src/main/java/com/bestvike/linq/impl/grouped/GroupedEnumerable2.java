8019b3d676c13107ffde3bcac345818f98c1065a
TimAndy <xuchonglei@126.com>
2018-05-02 14:29:10 +0800

    重命名

--- src/main/java/com/bestvike/linq/impl/grouped/GroupedEnumerable2.java
+++ src/main/java/com/bestvike/linq/impl/grouped/GroupedEnumerable2.java
@@ -15,10 +15,10 @@ import java.util.List;
  * Created by 许崇雷 on 2018-05-02.
  */
 public final class GroupedEnumerable2<TSource, TKey, TElement> implements IIListProvider<IGrouping<TKey, TElement>> {
-    private final IEnumerable<TSource> _source;
-    private final Func1<TSource, TKey> _keySelector;
-    private final Func1<TSource, TElement> _elementSelector;
-    private final IEqualityComparer<TKey> _comparer;
+    private final IEnumerable<TSource> source;
+    private final Func1<TSource, TKey> keySelector;
+    private final Func1<TSource, TElement> elementSelector;
+    private final IEqualityComparer<TKey> comparer;
 
     public GroupedEnumerable2(IEnumerable<TSource> source, Func1<TSource, TKey> keySelector, Func1<TSource, TElement> elementSelector, IEqualityComparer<TKey> comparer) {
         if (source == null)
@@ -28,32 +28,32 @@ public final class GroupedEnumerable2<TSource, TKey, TElement> implements IIList
         if (elementSelector == null)
             throw Errors.argumentNull("elementSelector");
 
-        this._source = source;
-        this._keySelector = keySelector;
-        this._elementSelector = elementSelector;
-        this._comparer = comparer;
+        this.source = source;
+        this.keySelector = keySelector;
+        this.elementSelector = elementSelector;
+        this.comparer = comparer;
     }
 
     public IEnumerator<IGrouping<TKey, TElement>> enumerator() {
-        return Lookup.create(this._source, this._keySelector, this._elementSelector, this._comparer).enumerator();
+        return Lookup.create(this.source, this.keySelector, this.elementSelector, this.comparer).enumerator();
     }
 
     public IGrouping<TKey, TElement>[] _toArray(Class<IGrouping<TKey, TElement>> clazz) {
-        IIListProvider<IGrouping<TKey, TElement>> lookup = Lookup.create(this._source, this._keySelector, this._elementSelector, this._comparer);
+        IIListProvider<IGrouping<TKey, TElement>> lookup = Lookup.create(this.source, this.keySelector, this.elementSelector, this.comparer);
         return lookup._toArray(clazz);
     }
 
     public Array<IGrouping<TKey, TElement>> _toArray() {
-        IIListProvider<IGrouping<TKey, TElement>> lookup = Lookup.create(this._source, this._keySelector, this._elementSelector, this._comparer);
+        IIListProvider<IGrouping<TKey, TElement>> lookup = Lookup.create(this.source, this.keySelector, this.elementSelector, this.comparer);
         return lookup._toArray();
     }
 
     public List<IGrouping<TKey, TElement>> _toList() {
-        IIListProvider<IGrouping<TKey, TElement>> lookup = Lookup.create(this._source, this._keySelector, this._elementSelector, this._comparer);
+        IIListProvider<IGrouping<TKey, TElement>> lookup = Lookup.create(this.source, this.keySelector, this.elementSelector, this.comparer);
         return lookup._toList();
     }
 
     public int _getCount(boolean onlyIfCheap) {
-        return onlyIfCheap ? -1 : Lookup.create(this._source, this._keySelector, this._elementSelector, this._comparer).getCount();
+        return onlyIfCheap ? -1 : Lookup.create(this.source, this.keySelector, this.elementSelector, this.comparer).getCount();
     }
 }
