98987ef8861a1b2e9d746b652e08ff907740fd81
TimAndy <xuchonglei@126.com>
2018-05-02 19:51:00 +0800

    完成 Lookup

---
+++ src/main/java/com/bestvike/linq/iterator/Lookup.java
@@ -0,0 +1,43 @@
+package com.bestvike.linq.iterator;
+
+import com.bestvike.collections.generic.IEqualityComparer;
+import com.bestvike.function.Func1;
+import com.bestvike.linq.IEnumerable;
+import com.bestvike.linq.ILookup;
+import com.bestvike.linq.exception.Errors;
+
+/**
+ * Created by 许崇雷 on 2018-05-02.
+ */
+public final class Lookup {
+    private Lookup() {
+    }
+
+    public static <TSource, TKey> ILookup<TKey, TSource> toLookup(IEnumerable<TSource> source, Func1<TSource, TKey> keySelector) {
+        return toLookup(source, keySelector, (IEqualityComparer<TKey>) null);
+    }
+
+    public static <TSource, TKey> ILookup<TKey, TSource> toLookup(IEnumerable<TSource> source, Func1<TSource, TKey> keySelector, IEqualityComparer<TKey> comparer) {
+        if (source == null)
+            throw Errors.argumentNull("source");
+        if (keySelector == null)
+            throw Errors.argumentNull("keySelector");
+
+        return com.bestvike.linq.impl.grouped.Lookup.create(source, keySelector, comparer);
+    }
+
+    public static <TSource, TKey, TElement> ILookup<TKey, TElement> toLookup(IEnumerable<TSource> source, Func1<TSource, TKey> keySelector, Func1<TSource, TElement> elementSelector) {
+        return toLookup(source, keySelector, elementSelector, null);
+    }
+
+    public static <TSource, TKey, TElement> ILookup<TKey, TElement> toLookup(IEnumerable<TSource> source, Func1<TSource, TKey> keySelector, Func1<TSource, TElement> elementSelector, IEqualityComparer<TKey> comparer) {
+        if (source == null)
+            throw Errors.argumentNull("source");
+        if (keySelector == null)
+            throw Errors.argumentNull("keySelector");
+        if (elementSelector == null)
+            throw Errors.argumentNull("elementSelector");
+
+        return com.bestvike.linq.impl.grouped.Lookup.create(source, keySelector, elementSelector, comparer);
+    }
+}
