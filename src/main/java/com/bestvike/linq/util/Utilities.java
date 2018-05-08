7fb03d697dc5a9bfda993d42b97efc4844219ab1
TimAndy <xuchonglei@126.com>
2018-05-08 14:35:19 +0800

    添加方法

--- src/main/java/com/bestvike/linq/util/Utilities.java
+++ src/main/java/com/bestvike/linq/util/Utilities.java
@@ -1,5 +1,7 @@
 package com.bestvike.linq.util;
 
+import com.bestvike.collections.generic.EqualityComparer;
+import com.bestvike.collections.generic.IEqualityComparer;
 import com.bestvike.function.Func1;
 
 /**
@@ -7,6 +9,25 @@ import com.bestvike.function.Func1;
  */
 public final class Utilities {
     private Utilities() {
+    }
+
+    public static <TSource> boolean areEqualityComparersEqual(IEqualityComparer<TSource> left, IEqualityComparer<TSource> right) {
+        if (left == right)
+            return true;
+
+        IEqualityComparer<TSource> defaultComparer = EqualityComparer.Default();
+        if (left == null) {
+            // Micro-opt: Typically it's impossible to get a different instance
+            // of the default comparer without reflection/serialization.
+            // Save a virtual method call to Equals in the common case.
+            return right == defaultComparer || right.equals(defaultComparer);
+        }
+
+        if (right == null) {
+            return left == defaultComparer || left.equals(defaultComparer);
+        }
+
+        return left.equals(right);
     }
 
     public static <TSource> Func1<TSource, Boolean> combinePredicates(Func1<TSource, Boolean> predicate1, Func1<TSource, Boolean> predicate2) {
