7df1405b1fa338e39ff9716b30ed94ce887dd558
TimAndy <xuchonglei@126.com>
2018-05-09 20:41:42 +0800

    删除无用文件

--- src/main/java/com/bestvike/linq/IListEnumerable.java
+++
@@ -1,8 +0,0 @@
-package com.bestvike.linq;
-
-/**
- * Created by 许崇雷 on 2017/7/20.
- */
-public interface IListEnumerable<TElement> extends ICollectionEnumerable<TElement> {
-    TElement internalGet(int index);
-}
