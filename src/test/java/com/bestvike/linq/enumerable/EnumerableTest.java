665ddb783a7a2131a25b5963dda5b9072c92923f
TimAndy <xuchonglei@126.com>
2018-05-11 16:49:17 +0800

    添加方法

--- src/test/java/com/bestvike/linq/enumerable/EnumerableTest.java
+++ src/test/java/com/bestvike/linq/enumerable/EnumerableTest.java
@@ -4,5 +4,11 @@ package com.bestvike.linq.enumerable;
  * Created by 许崇雷 on 2018-05-10.
  */
 public class EnumerableTest extends IteratorTest {
+    protected static Boolean IsEven(int num) {
+        return num % 2 == 0;
+    }
 
+    static Boolean IsNullOrEmpty(String value) {
+        return value == null || value.length() == 0;
+    }
 }
