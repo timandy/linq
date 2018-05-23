38686ad752660df36fb5277228650aa390dde783
TimAndy <xuchonglei@126.com>
2018-05-23 17:04:53 +0800

    调整代码

--- src/main/java/com/bestvike/linq/enumerable/DistinctBy.java
+++ src/main/java/com/bestvike/linq/enumerable/DistinctBy.java
@@ -78,10 +78,10 @@ final class DistinctByIterator<TSource, TKey> extends AbstractIterator<TSource> 
 
     @Override
     public void close() {
-        this.set = null;
         if (this.enumerator != null) {
             this.enumerator.close();
             this.enumerator = null;
+            this.set = null;
         }
         super.close();
     }
