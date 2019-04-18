6ef09536fb3a2b59a7baf8aaeac15b2e21c90a5c
TimAndy <xuchonglei@126.com>
2019-04-18 13:23:13 +0800

    删除暂不使用的构造和字段以减少内存占用

--- src/main/java/com/bestvike/linq/bridge/enumerator/CharSequenceEnumerator.java
+++ src/main/java/com/bestvike/linq/bridge/enumerator/CharSequenceEnumerator.java
@@ -7,31 +7,20 @@ import com.bestvike.linq.enumerable.AbstractEnumerator;
  */
 public final class CharSequenceEnumerator extends AbstractEnumerator<Character> {
     private final CharSequence source;
-    private final int endIndex;
-    private int index;
 
     public CharSequenceEnumerator(CharSequence source) {
-        this(source, 0, source.length());
-    }
-
-    public CharSequenceEnumerator(CharSequence source, int index, int count) {
         this.source = source;
-        this.index = index;
-        this.endIndex = Math.addExact(index, count);
     }
 
     @Override
     public boolean moveNext() {
-        switch (this.state) {
-            case 0:
-                if (this.index < this.endIndex) {
-                    this.current = this.source.charAt(this.index++);
-                    return true;
-                }
-                this.close();
-                return false;
-            default:
-                return false;
+        if (this.state == -1)
+            return false;
+        if (this.state < this.source.length()) {
+            this.current = this.source.charAt(this.state++);
+            return true;
         }
+        this.close();
+        return false;
     }
 }
