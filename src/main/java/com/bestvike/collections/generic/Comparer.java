6e6e2553ce48ad5b94d6e87acd6fc8f05f1ba1fb
TimAndy <xuchonglei@126.com>
2019-04-30 14:35:30 +0800

    Refactor Comparer

--- src/main/java/com/bestvike/collections/generic/Comparer.java
+++ src/main/java/com/bestvike/collections/generic/Comparer.java
@@ -1,19 +1,19 @@
 package com.bestvike.collections.generic;
 
+import com.bestvike.CultureInfo;
 import com.bestvike.IComparison;
 import com.bestvike.linq.exception.ExceptionArgument;
 import com.bestvike.linq.exception.ThrowHelper;
 
 import java.text.Collator;
 import java.util.Comparator;
-import java.util.Locale;
 
 /**
  * Created by 许崇雷 on 2017-07-18.
  */
 public final class Comparer<T> implements Comparator<T> {
-    private static volatile Comparer DEFAULT = new Comparer(Collator.getInstance());
-    private static volatile Comparer DEFAULT_INVARIANT = new Comparer(Collator.getInstance(Locale.ROOT));
+    private static final Comparer DEFAULT = new Comparer(null);
+    private static final Comparer DEFAULT_INVARIANT = new Comparer(CultureInfo.getInvariantCulture());
 
     private final Collator collator;
 
@@ -43,12 +43,6 @@ public final class Comparer<T> implements Comparator<T> {
         return new ComparisonComparer<>(comparison);
     }
 
-    public static void setDefault(Collator collator) {
-        if (collator == null)
-            ThrowHelper.throwArgumentNullException(ExceptionArgument.collator);
-        DEFAULT = new Comparer(collator);
-    }
-
     @Override
     public int compare(T x, T y) {
         if (x == y)
@@ -58,7 +52,7 @@ public final class Comparer<T> implements Comparator<T> {
         if (y == null)
             return 1;
         if (x instanceof String && y instanceof String)
-            return this.collator.compare((String) x, (String) y);
+            return (this.collator == null ? CultureInfo.getCurrentCulture() : this.collator).compare((String) x, (String) y);
         if (x instanceof Comparable)
             //noinspection unchecked
             return ((Comparable) x).compareTo(y);
