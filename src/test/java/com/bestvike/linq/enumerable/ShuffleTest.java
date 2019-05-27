6446caf5e5c4274bf798b21279dba84c0394d166
TimAndy <xuchonglei@126.com>
2019-05-27 15:40:36 +0800

    add shuffle testcase

---
+++ src/test/java/com/bestvike/linq/enumerable/ShuffleTest.java
@@ -0,0 +1,20 @@
+package com.bestvike.linq.enumerable;
+
+import com.bestvike.TestCase;
+import com.bestvike.collections.generic.Array;
+import com.bestvike.linq.IEnumerable;
+import com.bestvike.linq.Linq;
+import org.junit.Test;
+
+/**
+ * Created by 许崇雷 on 2019-05-27.
+ */
+public class ShuffleTest extends TestCase {
+    @Test
+    public void testShuffle() {
+        IEnumerable<Integer> source = Linq.range(0, 100);
+        Array<Integer> sourceArray = source.toArray();
+        assertNotEquals(sourceArray, source.shuffle());
+        assertNotEquals(sourceArray, source.shuffle(1));
+    }
+}
