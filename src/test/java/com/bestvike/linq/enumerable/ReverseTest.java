3aabd2e24292d2531449a05f0aff533232598dfe
TimAndy <xuchonglei@126.com>
2019-05-30 14:35:50 +0800

    add reverse testcase

--- src/test/java/com/bestvike/linq/enumerable/ReverseTest.java
+++ src/test/java/com/bestvike/linq/enumerable/ReverseTest.java
@@ -1,15 +1,100 @@
 package com.bestvike.linq.enumerable;
 
 import com.bestvike.TestCase;
+import com.bestvike.linq.IEnumerable;
+import com.bestvike.linq.IEnumerator;
 import com.bestvike.linq.Linq;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by 许崇雷 on 2018-05-10.
  */
 public class ReverseTest extends TestCase {
+    @Test
+    public void InvalidArguments() {
+        assertThrows(NullPointerException.class, () -> ((IEnumerable<String>) null).reverse());
+    }
+
+    @Test
+    public void Reverse() {
+        for (Object[] objects : this.ReverseData()) {
+            this.Reverse((IEnumerable<?>) objects[0]);
+        }
+    }
+
+    private <T> void Reverse(IEnumerable<T> source) {
+        List<T> expectedList = source.toList();
+        Collections.reverse(expectedList);
+        IEnumerable<T> expected = Linq.asEnumerable(expectedList);
+
+        IEnumerable<T> actual = source.reverse();
+
+        assertEquals(expected, actual);
+        assertEquals(expected.count(), actual.count()); // Count may be optimized.
+        assertEquals(expected, Linq.asEnumerable(actual.toArray()));
+        assertEquals(expected, Linq.asEnumerable(actual.toList()));
+
+        assertEquals(expected.firstOrDefault(), actual.firstOrDefault());
+        assertEquals(expected.lastOrDefault(), actual.lastOrDefault());
+
+        for (int i = 0; i < expected.count(); i++) {
+            assertEquals(expected.elementAt(i), actual.elementAt(i));
+
+            assertEquals(expected.skip(i), actual.skip(i));
+            assertEquals(expected.take(i), actual.take(i));
+        }
+
+        assertEquals(null, actual.elementAtOrDefault(-1));
+        assertEquals(null, actual.elementAtOrDefault(expected.count()));
+
+        assertEquals(expected, actual.select(a -> a));
+        assertEquals(expected, actual.where(a -> true));
+
+        assertEquals(actual, actual); // Repeat the enumeration against itself.
+    }
+
+    @Test
+    public void RunOnce() {
+        for (Object[] objects : this.ReverseData()) {
+            this.RunOnce((IEnumerable<?>) objects[0]);
+        }
+    }
+
+    private <T> void RunOnce(IEnumerable<T> source) {
+        List<T> expectedList = source.toList();
+        Collections.reverse(expectedList);
+        IEnumerable<T> expected = Linq.asEnumerable(expectedList);
+
+        IEnumerable<T> actual = source.runOnce().reverse();
+
+        assertEquals(expected, actual);
+    }
+
+    private IEnumerable<Object[]> ReverseData() {
+        IEnumerable<IEnumerable<?>> integers = Linq.asEnumerable(
+                Linq.asEnumerable(), // No elements.
+                Linq.asEnumerable(new int[]{1}), // One element.
+                Linq.asEnumerable(new int[]{9999, 0, 888, -1, 66, -777, 1, 2, -12345}), // Distinct elements.
+                Linq.asEnumerable(new int[]{-10, 0, 5, 0, 9, 100, 9}) // Some repeating elements.
+        );
+
+        return integers
+                .select(collection -> new Object[]{collection})
+                .concat(integers.select(c -> new Object[]{c.select(Object::toString)}));
+    }
+
+    @Test
+    public void ForcedToEnumeratorDoesntEnumerate() {
+        IEnumerable<Integer> iterator = NumberRangeGuaranteedNotCollectionType(0, 3).reverse();
+        // Don't insist on this behaviour, but check it's correct if it happens
+        IEnumerator<Integer> en = (IEnumerator<Integer>) iterator;
+        assertFalse(en != null && en.moveNext());
+    }
+
     @Test
     public void testReverse() {
         //null 在前,值相等的按原始顺序
