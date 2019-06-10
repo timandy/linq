4ef2bb6c3783aef780c8057bd3ef5cf846ca27f5
TimAndy <xuchonglei@126.com>
2019-06-10 18:24:18 +0800

    add takeLast testcase

--- src/test/java/com/bestvike/linq/enumerable/TakeLastTest.java
+++ src/test/java/com/bestvike/linq/enumerable/TakeLastTest.java
@@ -2,14 +2,97 @@ package com.bestvike.linq.enumerable;
 
 import com.bestvike.TestCase;
 import com.bestvike.linq.IEnumerable;
+import com.bestvike.linq.IEnumerator;
 import com.bestvike.linq.Linq;
 import com.bestvike.linq.entity.Department;
+import com.bestvike.ref;
 import org.junit.Test;
 
 /**
  * Created by 许崇雷 on 2018-05-17.
  */
 public class TakeLastTest extends TestCase {
+    @Test
+    public void TakeLast() {
+        for (Object[] objects : SkipTakeData.EnumerableData()) {
+            this.TakeLast((IEnumerable<Integer>) objects[0], (int) objects[1]);
+        }
+    }
+
+    private void TakeLast(IEnumerable<Integer> source, int count) {
+        assertAll(Linq.asEnumerable(TestCase.<Integer>IdentityTransforms()), transform -> {
+            IEnumerable<Integer> equivalent = transform.apply(source);
+
+            IEnumerable<Integer> expected = equivalent.reverse().take(count).reverse();
+            IEnumerable<Integer> actual = equivalent.takeLast(count);
+
+            assertEquals(expected, actual);
+            assertEquals(expected.count(), actual.count());
+            assertEquals(expected, actual.toArray());
+            assertEquals(expected, Linq.asEnumerable(actual.toList()));
+
+            assertEquals(expected.firstOrDefault(), actual.firstOrDefault());
+            assertEquals(expected.lastOrDefault(), actual.lastOrDefault());
+
+            assertAll(Linq.range(0, expected.count()), index -> {
+                assertEquals(expected.elementAt(index), actual.elementAt(index));
+            });
+
+            assertEquals(null, actual.elementAtOrDefault(-1));
+            assertEquals(null, actual.elementAtOrDefault(actual.count()));
+        });
+    }
+
+    @Test
+    public void EvaluationBehavior() {
+        for (Object[] objects : SkipTakeData.EvaluationBehaviorData()) {
+            this.EvaluationBehavior((int) objects[0]);
+        }
+    }
+
+    private void EvaluationBehavior(int count) {
+        ref<Integer> index = ref.init(0);
+        int limit = count * 2;
+
+        IEnumerable<Integer> source = new DelegateIterator<>(
+                () -> index.value++ != limit, // Stop once we go past the limit.
+                () -> index.value, // Yield from 1 up to the limit, inclusive.
+                () -> index.value ^= Integer.MIN_VALUE);
+
+        IEnumerator<Integer> iterator = source.takeLast(count).enumerator();
+        assertEquals(0, index.value); // Nothing should be done before MoveNext is called.
+
+        for (int i = 1; i <= count; i++) {
+            assertTrue(iterator.moveNext());
+            assertEquals(count + i, iterator.current());
+
+            // After the first MoveNext call to the enumerator, everything should be evaluated and the enumerator
+            // should be disposed.
+            assertEquals(Integer.MIN_VALUE, index.value & Integer.MIN_VALUE);
+            assertEquals(limit + 1, index.value & Integer.MAX_VALUE);
+        }
+
+        assertFalse(iterator.moveNext());
+
+        // Unlike SkipLast, TakeLast can tell straightaway that it can return a sequence with no elements if count <= 0.
+        // The enumerable it returns is a specialized empty iterator that has no connections to the source. Hence,
+        // after MoveNext returns false under those circumstances, it won't invoke Dispose on our enumerator.
+        int expected = count <= 0 ? 0 : Integer.MIN_VALUE;
+        assertEquals(expected, index.value & Integer.MIN_VALUE);
+    }
+
+    @Test
+    public void RunOnce() {
+        for (Object[] objects : SkipTakeData.EnumerableData()) {
+            this.RunOnce((IEnumerable<Integer>) objects[0], (int) objects[1]);
+        }
+    }
+
+    private void RunOnce(IEnumerable<Integer> source, int count) {
+        IEnumerable<Integer> expected = source.takeLast(count);
+        assertEquals(expected, source.takeLast(count).runOnce());
+    }
+
     @Test
     public void testTakeLast() {
         assertEquals(1, Linq.asEnumerable(depts).takeLast(1).count());
