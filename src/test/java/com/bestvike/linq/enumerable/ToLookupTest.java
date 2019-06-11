551bdd059ff3ce442d1e8ea2165f18008ce40030
TimAndy <xuchonglei@126.com>
2019-06-11 16:36:46 +0800

    add toLookup testcase

--- src/test/java/com/bestvike/linq/enumerable/ToLookupTest.java
+++ src/test/java/com/bestvike/linq/enumerable/ToLookupTest.java
@@ -1,19 +1,275 @@
 package com.bestvike.linq.enumerable;
 
 import com.bestvike.TestCase;
+import com.bestvike.ValueType;
+import com.bestvike.collections.generic.EqualityComparer;
 import com.bestvike.collections.generic.IEqualityComparer;
+import com.bestvike.function.Func1;
+import com.bestvike.linq.IEnumerable;
+import com.bestvike.linq.IEnumerator;
 import com.bestvike.linq.IGrouping;
 import com.bestvike.linq.ILookup;
 import com.bestvike.linq.Linq;
 import com.bestvike.linq.entity.Employee;
+import com.bestvike.linq.exception.ArgumentNullException;
+import com.bestvike.tuple.Tuple;
+import com.bestvike.tuple.Tuple2;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 /**
  * Created by 许崇雷 on 2018-05-10.
  */
 public class ToLookupTest extends TestCase {
+    private static <K, T> void AssertMatches(IEnumerable<K> keys, IEnumerable<T> elements, ILookup<K, T> lookup) {
+        assertNotNull(lookup);
+        assertNotNull(keys);
+        assertNotNull(elements);
+
+        int num = 0;
+        try (IEnumerator<K> keyEnumerator = keys.enumerator();
+             IEnumerator<T> elEnumerator = elements.enumerator()) {
+            while (keyEnumerator.moveNext()) {
+                assertTrue(lookup.containsKey(keyEnumerator.current()));
+
+                for (T e : lookup.get(keyEnumerator.current())) {
+                    assertTrue(elEnumerator.moveNext());
+                    assertEquals(e, elEnumerator.current());
+                }
+                ++num;
+            }
+            assertFalse(elEnumerator.moveNext());
+        }
+        assertEquals(num, lookup.getCount());
+    }
+
+    @Test
+    public void SameResultsRepeatCall() {
+        IEnumerable<String> q1 = Linq.asEnumerable("Alen", "Felix", null, null, "X", "Have Space", "Clinton", "");
+        IEnumerable<Integer> q2 = Linq.asEnumerable(new int[]{55, 49, 9, -100, 24, 25, -1, 0});
+        IEnumerable<Tuple2<String, Integer>> q = q1.selectMany(a -> q2, (x3, x4) -> Tuple.create(x3, x4));
+
+
+        assertEquals(q.toLookup(e -> e.getItem1()), q.toLookup(e -> e.getItem1()));
+    }
+
+    @Test
+    public void NullKeyIncluded() {
+        String[] key = {"Chris", "Bob", null, "Tim"};
+        int[] element = {50, 95, 55, 90};
+        IEnumerable<NameScore> source = Linq.asEnumerable(key).zip(Linq.asEnumerable(element), (k, e) -> new NameScore(k, e));
+
+        AssertMatches(Linq.asEnumerable(key), source, source.toLookup(e -> e.Name));
+    }
+
+    @Test
+    public void OneElementCustomComparer() {
+        String[] key = {"Chris"};
+        int[] element = {50};
+        NameScore[] source = new NameScore[]{new NameScore("risCh", 50)};
+
+        AssertMatches(Linq.asEnumerable(key), Linq.asEnumerable(source), Linq.asEnumerable(source).toLookup(e -> e.Name, new AnagramEqualityComparer()));
+    }
+
+    @Test
+    public void UniqueElementsElementSelector() {
+        String[] key = {"Chris", "Prakash", "Tim", "Robert", "Brian"};
+        int[] element = {50, 100, 95, 60, 80};
+        NameScore[] source = new NameScore[]
+                {
+                        new NameScore(key[0], element[0]),
+                        new NameScore(key[1], element[1]),
+                        new NameScore(key[2], element[2]),
+                        new NameScore(key[3], element[3]),
+                        new NameScore(key[4], element[4])
+                };
+
+        AssertMatches(Linq.asEnumerable(key), Linq.asEnumerable(element), Linq.asEnumerable(source).toLookup(e -> e.Name, e -> e.Score));
+    }
+
+    @Test
+    public void DuplicateKeys() {
+        String[] key = {"Chris", "Prakash", "Robert"};
+        int[] element = {50, 80, 100, 95, 99, 56};
+        NameScore[] source = new NameScore[]
+                {
+                        new NameScore(key[0], element[0]),
+                        new NameScore(key[1], element[2]),
+                        new NameScore(key[2], element[5]),
+                        new NameScore(key[1], element[3]),
+                        new NameScore(key[0], element[1]),
+                        new NameScore(key[1], element[4])
+                };
+
+        AssertMatches(Linq.asEnumerable(key), Linq.asEnumerable(element), Linq.asEnumerable(source).toLookup(e -> e.Name, e -> e.Score, new AnagramEqualityComparer()));
+    }
+
+    @Test
+    public void RunOnce() {
+        String[] key = {"Chris", "Prakash", "Robert"};
+        int[] element = {50, 80, 100, 95, 99, 56};
+        NameScore[] source = new NameScore[]
+                {
+                        new NameScore(key[0], element[0]),
+                        new NameScore(key[1], element[2]),
+                        new NameScore(key[2], element[5]),
+                        new NameScore(key[1], element[3]),
+                        new NameScore(key[0], element[1]),
+                        new NameScore(key[1], element[4])
+                };
+
+        AssertMatches(Linq.asEnumerable(key), Linq.asEnumerable(element), Linq.asEnumerable(source).runOnce().toLookup(e -> e.Name, e -> e.Score, new AnagramEqualityComparer()));
+    }
+
+    @Test
+    public void Count() {
+        String[] key = {"Chris", "Prakash", "Robert"};
+        int[] element = {50, 80, 100, 95, 99, 56};
+        NameScore[] source = new NameScore[]
+                {
+                        new NameScore(key[0], element[0]),
+                        new NameScore(key[1], element[2]),
+                        new NameScore(key[2], element[5]),
+                        new NameScore(key[1], element[3]),
+                        new NameScore(key[0], element[1]),
+                        new NameScore(key[1], element[4])
+                };
+
+        assertEquals(3, Linq.asEnumerable(source).toLookup(e -> e.Name, e -> e.Score).count());
+    }
+
+    @Test
+    public void EmptySource() {
+        String[] key = {};
+        int[] element = {};
+        IEnumerable<NameScore> source = Linq.asEnumerable(key).zip(Linq.asEnumerable(element), (k, e) -> new NameScore(k, e));
+
+        AssertMatches(Linq.asEnumerable(key), Linq.asEnumerable(element), Linq.asEnumerable(source).toLookup(e -> e.Name, e -> e.Score, new AnagramEqualityComparer()));
+    }
+
+    @Test
+    public void SingleNullKeyAndElement() {
+        String[] key = {null};
+        String[] element = {null};
+        String[] source = new String[]{null};
+
+        AssertMatches(Linq.asEnumerable(key), Linq.asEnumerable(element), Linq.asEnumerable(source).toLookup(e -> e, e -> e, EqualityComparer.Default()));
+    }
+
+    @Test
+    public void NullSource() {
+        IEnumerable<Integer> source = null;
+        assertThrows(NullPointerException.class, () -> source.toLookup(i -> i / 10));
+    }
+
+    @Test
+    public void NullSourceExplicitComparer() {
+        IEnumerable<Integer> source = null;
+        assertThrows(NullPointerException.class, () -> source.toLookup(i -> i / 10, EqualityComparer.Default()));
+    }
+
+    @Test
+    public void NullSourceElementSelector() {
+        IEnumerable<Integer> source = null;
+        assertThrows(NullPointerException.class, () -> source.toLookup(i -> i / 10, i -> i + 2));
+    }
+
+    @Test
+    public void NullSourceElementSelectorExplicitComparer() {
+        IEnumerable<Integer> source = null;
+        assertThrows(NullPointerException.class, () -> source.toLookup(i -> i / 10, i -> i + 2, EqualityComparer.Default()));
+    }
+
+    @Test
+    public void NullKeySelector() {
+        Func1<Integer, Integer> keySelector = null;
+        assertThrows(ArgumentNullException.class, () -> Linq.range(0, 1000).toLookup(keySelector));
+    }
+
+    @Test
+    public void NullKeySelectorExplicitComparer() {
+        Func1<Integer, Integer> keySelector = null;
+        assertThrows(ArgumentNullException.class, () -> Linq.range(0, 1000).toLookup(keySelector, EqualityComparer.Default()));
+    }
+
+    @Test
+    public void NullKeySelectorElementSelector() {
+        Func1<Integer, Integer> keySelector = null;
+        assertThrows(ArgumentNullException.class, () -> Linq.range(0, 1000).toLookup(keySelector, i -> i + 2));
+    }
+
+    @Test
+    public void NullKeySelectorElementSelectorExplicitComparer() {
+        Func1<Integer, Integer> keySelector = null;
+        assertThrows(ArgumentNullException.class, () -> Linq.range(0, 1000).toLookup(keySelector, i -> i + 2, EqualityComparer.Default()));
+    }
+
+    @Test
+    public void NullElementSelector() {
+        Func1<Integer, Integer> elementSelector = null;
+        assertThrows(ArgumentNullException.class, () -> Linq.range(0, 1000).toLookup(i -> i / 10, elementSelector));
+    }
+
+    @Test
+    public void NullElementSelectorExplicitComparer() {
+        Func1<Integer, Integer> elementSelector = null;
+        assertThrows(ArgumentNullException.class, () -> Linq.range(0, 1000).toLookup(i -> i / 10, elementSelector, EqualityComparer.Default()));
+    }
+
+    @Test
+    public void ApplyResultSelectorForGroup() {
+        this.ApplyResultSelectorForGroup(1);
+        this.ApplyResultSelectorForGroup(2);
+        this.ApplyResultSelectorForGroup(3);
+    }
+
+    private void ApplyResultSelectorForGroup(int enumType) {
+        //Create test data
+        List<Role> roles = Arrays.asList(
+                new Role(1),
+                new Role(2),
+                new Role(3)
+        );
+
+        IEnumerable<Membership> memberships = Linq.range(0, 50).select(i -> new Membership(i,
+                roles.get(i % 3),
+                i % 3 == 0));
+
+        //Run actual test
+        IEnumerable<RoleMetadata> grouping = memberships.groupBy(
+                m -> m.Role,
+                (role, mems) -> new RoleMetadata
+                        (
+                                role,
+                                mems.count(m -> m.CountMe),
+                                mems.count(m -> !m.CountMe)));
+
+        IEnumerable<RoleMetadata> result;
+        switch (enumType) {
+            case 1:
+                result = Linq.asEnumerable(grouping.toList());
+                break;
+            case 2:
+                result = grouping.toArray();
+                break;
+            default:
+                result = grouping;
+                break;
+        }
+
+        RoleMetadata[] expected = new RoleMetadata[]
+                {
+                        new RoleMetadata(new Role(1), 17, 0),
+                        new RoleMetadata(new Role(2), 0, 17),
+                        new RoleMetadata(new Role(3), 0, 16)
+                };
+
+        assertEquals(Linq.asEnumerable(expected), result);
+    }
+
     @Test
     public void testToLookup() {
         ILookup<Integer, Employee> lookup = Linq.asEnumerable(emps).toLookup(emp -> emp.deptno);
@@ -192,5 +448,48 @@ public class ToLookupTest extends TestCase {
             }
         }
         assertEquals(1, n2);
+    }
+
+
+    private static class NameScore extends ValueType {
+        final String Name;
+        final int Score;
+
+        private NameScore(String name, int score) {
+            this.Name = name;
+            this.Score = score;
+        }
+    }
+
+    private static class Membership extends ValueType {
+        final int Id;
+        final Role Role;
+        final boolean CountMe;
+
+        Membership(int id, Role role, boolean countMe) {
+            this.Id = id;
+            this.Role = role;
+            this.CountMe = countMe;
+        }
+    }
+
+    private static class Role extends ValueType {
+        final int Id;
+
+        Role(int id) {
+            this.Id = id;
+        }
+    }
+
+    public static class RoleMetadata extends ValueType {
+        final Role Role;
+        final int CountA;
+        final int CountrB;
+
+        RoleMetadata(Role role, int countA, int countrB) {
+            this.Role = role;
+            this.CountA = countA;
+            this.CountrB = countrB;
+        }
     }
 }
