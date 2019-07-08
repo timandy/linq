ca9e3baa123f9ee1192a12bbe454a278da5d37e9
TimAndy <xuchonglei@126.com>
2019-07-08 18:17:10 +0800

    make IList extends RandomAccess

--- src/main/java/com/bestvike/collections/generic/IList.java
+++ src/main/java/com/bestvike/collections/generic/IList.java
@@ -1,9 +1,11 @@
 package com.bestvike.collections.generic;
 
+import java.util.RandomAccess;
+
 /**
  * Created by 许崇雷 on 2018-04-27.
  */
-public interface IList<T> extends ICollection<T> {
+public interface IList<T> extends ICollection<T>, RandomAccess {
     T get(int index);
 
     int _indexOf(T item);
