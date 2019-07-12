adf5502edd0e6cb0c57a0da70dade69467a82d48
TimAndy <xuchonglei@126.com>
2019-07-12 11:32:46 +0800

    add findIndex and findLastIndex testcase

--- src/test/java/com/bestvike/linq/enumerable/FindIndexTest.java
+++ src/test/java/com/bestvike/linq/enumerable/FindIndexTest.java
@@ -5,8 +5,26 @@ import com.bestvike.collections.generic.Array;
 import com.bestvike.function.Predicate1;
 import com.bestvike.linq.IEnumerable;
 import com.bestvike.linq.Linq;
+import com.bestvike.linq.adapter.enumerable.ArrayListEnumerable;
+import com.bestvike.linq.adapter.enumerable.BooleanArrayEnumerable;
+import com.bestvike.linq.adapter.enumerable.ByteArrayEnumerable;
+import com.bestvike.linq.adapter.enumerable.CharSequenceEnumerable;
+import com.bestvike.linq.adapter.enumerable.CharacterArrayEnumerable;
+import com.bestvike.linq.adapter.enumerable.DoubleArrayEnumerable;
+import com.bestvike.linq.adapter.enumerable.FloatArrayEnumerable;
+import com.bestvike.linq.adapter.enumerable.GenericArrayEnumerable;
+import com.bestvike.linq.adapter.enumerable.IntegerArrayEnumerable;
+import com.bestvike.linq.adapter.enumerable.LinkedListEnumerable;
+import com.bestvike.linq.adapter.enumerable.LongArrayEnumerable;
+import com.bestvike.linq.adapter.enumerable.ShortArrayEnumerable;
+import com.bestvike.linq.adapter.enumerable.SingletonEnumerable;
 import com.bestvike.linq.exception.ArgumentNullException;
+import com.bestvike.tuple.Tuple;
+import com.bestvike.tuple.Tuple2;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.LinkedList;
 
 /**
  * Created by 许崇雷 on 2019-06-17.
@@ -80,6 +98,101 @@ public class FindIndexTest extends TestCase {
     public void NullPredicate_ThrowsArgumentNullException() {
         Predicate1<Integer> predicate = null;
         assertThrows(ArgumentNullException.class, () -> Linq.range(0, 3).findIndex(predicate));
+    }
+
+    @Test
+    public void testFindIndex() {
+        assertEquals(-1, Linq.<Integer>empty().findIndex(x -> x == 0));
+
+        assertEquals(0, Linq.singleton(1).findIndex(x -> x == 1));
+        assertEquals(-1, Linq.singleton(1).findIndex(x -> x == 0));
+
+        assertEquals(0, Linq.of(new boolean[]{true, true, true}).findIndex(x -> x));
+        assertEquals(-1, Linq.of(new boolean[]{true, true, true}).findIndex(x -> !x));
+
+        assertEquals(0, Linq.of(new byte[]{1, 2, 3, 2, 1}).findIndex(x -> x == 1));
+        assertEquals(-1, Linq.of(new byte[]{1, 2, 3, 2, 1}).findIndex(x -> x == 4));
+
+        assertEquals(0, Linq.of(new short[]{1, 2, 3, 2, 1}).findIndex(x -> x == 1));
+        assertEquals(-1, Linq.of(new short[]{1, 2, 3, 2, 1}).findIndex(x -> x == 4));
+
+        assertEquals(0, Linq.of(new int[]{1, 2, 3, 2, 1}).findIndex(x -> x == 1));
+        assertEquals(-1, Linq.of(new int[]{1, 2, 3, 2, 1}).findIndex(x -> x == 4));
+
+        assertEquals(0, Linq.of(new long[]{1L, 2L, 3L, 2L, 1L}).findIndex(x -> x == 1));
+        assertEquals(-1, Linq.of(new long[]{1L, 2L, 3L, 2L, 1L}).findIndex(x -> x == 4));
+
+        assertEquals(0, Linq.of(new float[]{1f, 2f, 3f, 2f, 1f}).findIndex(x -> x == 1f));
+        assertEquals(-1, Linq.of(new float[]{1f, 2f, 3f, 2f, 1f}).findIndex(x -> x == 4f));
+
+        assertEquals(0, Linq.of(new double[]{1d, 2d, 3d, 2d, 1d}).findIndex(x -> x == 1d));
+        assertEquals(-1, Linq.of(new double[]{1d, 2d, 3d, 2d, 1d}).findIndex(x -> x == 4d));
+
+        assertEquals(0, Linq.of(new char[]{'1', '2', '3', '2', '1'}).findIndex(x -> x == '1'));
+        assertEquals(-1, Linq.of(new char[]{'1', '2', '3', '2', '1'}).findIndex(x -> x == '4'));
+
+        assertEquals(0, Linq.of("12321").findIndex(x -> x == '1'));
+        assertEquals(-1, Linq.of("12321").findIndex(x -> x == '4'));
+
+        assertEquals(0, Linq.of(1, 2, 3, 2, 1).findIndex(x -> x == 1));
+        assertEquals(-1, Linq.of(1, 2, 3, 2, 1).findIndex(x -> x == 4));
+
+        assertEquals(0, Linq.of(Arrays.asList(1, 2, 3, 2, 1)).findIndex(x -> x == 1));
+        assertEquals(-1, Linq.of(Arrays.asList(1, 2, 3, 2, 1)).findIndex(x -> x == 4));
+
+        assertEquals(0, Linq.of(new LinkedList<>(Arrays.asList(1, 2, 3, 2, 1))).findIndex(x -> x == 1));
+        assertEquals(-1, Linq.of(new LinkedList<>(Arrays.asList(1, 2, 3, 2, 1))).findIndex(x -> x == 4));
+
+        assertEquals(1, Linq.of(Tuple.create(1, "1"), Tuple.create(2, "2"), Tuple.create(2, "3"), Tuple.create(2, "3")).groupBy(Tuple2::getItem1).elementAt(1).findIndex(a -> a.getItem2().equals("3")));
+        assertEquals(-1, Linq.of(Tuple.create(1, "1"), Tuple.create(2, "2"), Tuple.create(2, "3"), Tuple.create(2, "3")).groupBy(Tuple2::getItem1).elementAt(1).findIndex(a -> a.getItem2().equals("4")));
+
+        assertEquals(0, Linq.of(1, 2, 3, 2, 1).runOnce().findIndex(x -> x == 1));
+        assertEquals(-1, Linq.of(1, 2, 3, 2, 1).runOnce().findIndex(x -> x == 4));
+    }
+
+    @Test
+    public void testFindIndexNullPredicate() {
+        assertThrows(ArgumentNullException.class, () -> ((SingletonEnumerable<Integer>) Linq.singleton(1))._findIndex(null));
+        assertThrows(ArgumentNullException.class, () -> ((SingletonEnumerable<Integer>) Linq.singleton(1))._findIndex(null));
+
+        assertThrows(ArgumentNullException.class, () -> ((BooleanArrayEnumerable) Linq.of(new boolean[]{true, true, true}))._findIndex(null));
+        assertThrows(ArgumentNullException.class, () -> ((BooleanArrayEnumerable) Linq.of(new boolean[]{true, true, true}))._findIndex(null));
+
+        assertThrows(ArgumentNullException.class, () -> ((ByteArrayEnumerable) Linq.of(new byte[]{1, 2, 3, 2, 1}))._findIndex(null));
+        assertThrows(ArgumentNullException.class, () -> ((ByteArrayEnumerable) Linq.of(new byte[]{1, 2, 3, 2, 1}))._findIndex(null));
+
+        assertThrows(ArgumentNullException.class, () -> ((ShortArrayEnumerable) Linq.of(new short[]{1, 2, 3, 2, 1}))._findIndex(null));
+        assertThrows(ArgumentNullException.class, () -> ((ShortArrayEnumerable) Linq.of(new short[]{1, 2, 3, 2, 1}))._findIndex(null));
+
+        assertThrows(ArgumentNullException.class, () -> ((IntegerArrayEnumerable) Linq.of(new int[]{1, 2, 3, 2, 1}))._findIndex(null));
+        assertThrows(ArgumentNullException.class, () -> ((IntegerArrayEnumerable) Linq.of(new int[]{1, 2, 3, 2, 1}))._findIndex(null));
+
+        assertThrows(ArgumentNullException.class, () -> ((LongArrayEnumerable) Linq.of(new long[]{1L, 2L, 3L, 2L, 1L}))._findIndex(null));
+        assertThrows(ArgumentNullException.class, () -> ((LongArrayEnumerable) Linq.of(new long[]{1L, 2L, 3L, 2L, 1L}))._findIndex(null));
+
+        assertThrows(ArgumentNullException.class, () -> ((FloatArrayEnumerable) Linq.of(new float[]{1f, 2f, 3f, 2f, 1f}))._findIndex(null));
+        assertThrows(ArgumentNullException.class, () -> ((FloatArrayEnumerable) Linq.of(new float[]{1f, 2f, 3f, 2f, 1f}))._findIndex(null));
+
+        assertThrows(ArgumentNullException.class, () -> ((DoubleArrayEnumerable) Linq.of(new double[]{1d, 2d, 3d, 2d, 1d}))._findIndex(null));
+        assertThrows(ArgumentNullException.class, () -> ((DoubleArrayEnumerable) Linq.of(new double[]{1d, 2d, 3d, 2d, 1d}))._findIndex(null));
+
+        assertThrows(ArgumentNullException.class, () -> ((CharacterArrayEnumerable) Linq.of(new char[]{'1', '2', '3', '2', '1'}))._findIndex(null));
+        assertThrows(ArgumentNullException.class, () -> ((CharacterArrayEnumerable) Linq.of(new char[]{'1', '2', '3', '2', '1'}))._findIndex(null));
+
+        assertThrows(ArgumentNullException.class, () -> ((CharSequenceEnumerable) Linq.of("12321"))._findIndex(null));
+        assertThrows(ArgumentNullException.class, () -> ((CharSequenceEnumerable) Linq.of("12321"))._findIndex(null));
+
+        assertThrows(ArgumentNullException.class, () -> ((GenericArrayEnumerable<Integer>) Linq.of(1, 2, 3, 2, 1))._findIndex(null));
+        assertThrows(ArgumentNullException.class, () -> ((GenericArrayEnumerable<Integer>) Linq.of(1, 2, 3, 2, 1))._findIndex(null));
+
+        assertThrows(ArgumentNullException.class, () -> ((ArrayListEnumerable<Integer>) Linq.of(Arrays.asList(1, 2, 3, 2, 1)))._findIndex(null));
+        assertThrows(ArgumentNullException.class, () -> ((ArrayListEnumerable<Integer>) Linq.of(Arrays.asList(1, 2, 3, 2, 1)))._findIndex(null));
+
+        assertThrows(ArgumentNullException.class, () -> ((LinkedListEnumerable<Integer>) Linq.of(new LinkedList<>(Arrays.asList(1, 2, 3, 2, 1))))._findIndex(null));
+        assertThrows(ArgumentNullException.class, () -> ((LinkedListEnumerable<Integer>) Linq.of(new LinkedList<>(Arrays.asList(1, 2, 3, 2, 1))))._findIndex(null));
+
+        assertThrows(ArgumentNullException.class, () -> ((RunOnceLinkedList<Integer>) Linq.of(1, 2, 3, 2, 1).runOnce())._findIndex(null));
+        assertThrows(ArgumentNullException.class, () -> ((RunOnceLinkedList<Integer>) Linq.of(1, 2, 3, 2, 1).runOnce())._findIndex(null));
     }
 
     @Test
--- src/test/java/com/bestvike/linq/enumerable/FindLastIndexTest.java
+++ src/test/java/com/bestvike/linq/enumerable/FindLastIndexTest.java
@@ -5,8 +5,26 @@ import com.bestvike.collections.generic.Array;
 import com.bestvike.function.Predicate1;
 import com.bestvike.linq.IEnumerable;
 import com.bestvike.linq.Linq;
+import com.bestvike.linq.adapter.enumerable.ArrayListEnumerable;
+import com.bestvike.linq.adapter.enumerable.BooleanArrayEnumerable;
+import com.bestvike.linq.adapter.enumerable.ByteArrayEnumerable;
+import com.bestvike.linq.adapter.enumerable.CharSequenceEnumerable;
+import com.bestvike.linq.adapter.enumerable.CharacterArrayEnumerable;
+import com.bestvike.linq.adapter.enumerable.DoubleArrayEnumerable;
+import com.bestvike.linq.adapter.enumerable.FloatArrayEnumerable;
+import com.bestvike.linq.adapter.enumerable.GenericArrayEnumerable;
+import com.bestvike.linq.adapter.enumerable.IntegerArrayEnumerable;
+import com.bestvike.linq.adapter.enumerable.LinkedListEnumerable;
+import com.bestvike.linq.adapter.enumerable.LongArrayEnumerable;
+import com.bestvike.linq.adapter.enumerable.ShortArrayEnumerable;
+import com.bestvike.linq.adapter.enumerable.SingletonEnumerable;
 import com.bestvike.linq.exception.ArgumentNullException;
+import com.bestvike.tuple.Tuple;
+import com.bestvike.tuple.Tuple2;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.LinkedList;
 
 /**
  * Created by 许崇雷 on 2019-06-17.
@@ -80,6 +98,101 @@ public class FindLastIndexTest extends TestCase {
     public void NullPredicate_ThrowsArgumentNullException() {
         Predicate1<Integer> predicate = null;
         assertThrows(ArgumentNullException.class, () -> Linq.range(0, 3).findLastIndex(predicate));
+    }
+
+    @Test
+    public void testFindLastIndex() {
+        assertEquals(-1, Linq.<Integer>empty().findLastIndex(x -> x == 0));
+
+        assertEquals(0, Linq.singleton(1).findLastIndex(x -> x == 1));
+        assertEquals(-1, Linq.singleton(1).findLastIndex(x -> x == 0));
+
+        assertEquals(2, Linq.of(new boolean[]{true, true, true}).findLastIndex(x -> x));
+        assertEquals(-1, Linq.of(new boolean[]{true, true, true}).findLastIndex(x -> !x));
+
+        assertEquals(4, Linq.of(new byte[]{1, 2, 3, 2, 1}).findLastIndex(x -> x == 1));
+        assertEquals(-1, Linq.of(new byte[]{1, 2, 3, 2, 1}).findLastIndex(x -> x == 4));
+
+        assertEquals(4, Linq.of(new short[]{1, 2, 3, 2, 1}).findLastIndex(x -> x == 1));
+        assertEquals(-1, Linq.of(new short[]{1, 2, 3, 2, 1}).findLastIndex(x -> x == 4));
+
+        assertEquals(4, Linq.of(new int[]{1, 2, 3, 2, 1}).findLastIndex(x -> x == 1));
+        assertEquals(-1, Linq.of(new int[]{1, 2, 3, 2, 1}).findLastIndex(x -> x == 4));
+
+        assertEquals(4, Linq.of(new long[]{1L, 2L, 3L, 2L, 1L}).findLastIndex(x -> x == 1));
+        assertEquals(-1, Linq.of(new long[]{1L, 2L, 3L, 2L, 1L}).findLastIndex(x -> x == 4));
+
+        assertEquals(4, Linq.of(new float[]{1f, 2f, 3f, 2f, 1f}).findLastIndex(x -> x == 1f));
+        assertEquals(-1, Linq.of(new float[]{1f, 2f, 3f, 2f, 1f}).findLastIndex(x -> x == 4f));
+
+        assertEquals(4, Linq.of(new double[]{1d, 2d, 3d, 2d, 1d}).findLastIndex(x -> x == 1d));
+        assertEquals(-1, Linq.of(new double[]{1d, 2d, 3d, 2d, 1d}).findLastIndex(x -> x == 4d));
+
+        assertEquals(4, Linq.of(new char[]{'1', '2', '3', '2', '1'}).findLastIndex(x -> x == '1'));
+        assertEquals(-1, Linq.of(new char[]{'1', '2', '3', '2', '1'}).findLastIndex(x -> x == '4'));
+
+        assertEquals(4, Linq.of("12321").findLastIndex(x -> x == '1'));
+        assertEquals(-1, Linq.of("12321").findLastIndex(x -> x == '4'));
+
+        assertEquals(4, Linq.of(1, 2, 3, 2, 1).findLastIndex(x -> x == 1));
+        assertEquals(-1, Linq.of(1, 2, 3, 2, 1).findLastIndex(x -> x == 4));
+
+        assertEquals(4, Linq.of(Arrays.asList(1, 2, 3, 2, 1)).findLastIndex(x -> x == 1));
+        assertEquals(-1, Linq.of(Arrays.asList(1, 2, 3, 2, 1)).findLastIndex(x -> x == 4));
+
+        assertEquals(4, Linq.of(new LinkedList<>(Arrays.asList(1, 2, 3, 2, 1))).findLastIndex(x -> x == 1));
+        assertEquals(-1, Linq.of(new LinkedList<>(Arrays.asList(1, 2, 3, 2, 1))).findLastIndex(x -> x == 4));
+
+        assertEquals(2, Linq.of(Tuple.create(1, "1"), Tuple.create(2, "2"), Tuple.create(2, "3"), Tuple.create(2, "3")).groupBy(Tuple2::getItem1).elementAt(1).findLastIndex(a -> a.getItem2().equals("3")));
+        assertEquals(-1, Linq.of(Tuple.create(1, "1"), Tuple.create(2, "2"), Tuple.create(2, "3"), Tuple.create(2, "3")).groupBy(Tuple2::getItem1).elementAt(1).findLastIndex(a -> a.getItem2().equals("4")));
+
+        assertEquals(4, Linq.of(1, 2, 3, 2, 1).runOnce().findLastIndex(x -> x == 1));
+        assertEquals(-1, Linq.of(1, 2, 3, 2, 1).runOnce().findLastIndex(x -> x == 4));
+    }
+
+    @Test
+    public void testFindLastIndexNullPredicate() {
+        assertThrows(ArgumentNullException.class, () -> ((SingletonEnumerable<Integer>) Linq.singleton(1))._findLastIndex(null));
+        assertThrows(ArgumentNullException.class, () -> ((SingletonEnumerable<Integer>) Linq.singleton(1))._findLastIndex(null));
+
+        assertThrows(ArgumentNullException.class, () -> ((BooleanArrayEnumerable) Linq.of(new boolean[]{true, true, true}))._findLastIndex(null));
+        assertThrows(ArgumentNullException.class, () -> ((BooleanArrayEnumerable) Linq.of(new boolean[]{true, true, true}))._findLastIndex(null));
+
+        assertThrows(ArgumentNullException.class, () -> ((ByteArrayEnumerable) Linq.of(new byte[]{1, 2, 3, 2, 1}))._findLastIndex(null));
+        assertThrows(ArgumentNullException.class, () -> ((ByteArrayEnumerable) Linq.of(new byte[]{1, 2, 3, 2, 1}))._findLastIndex(null));
+
+        assertThrows(ArgumentNullException.class, () -> ((ShortArrayEnumerable) Linq.of(new short[]{1, 2, 3, 2, 1}))._findLastIndex(null));
+        assertThrows(ArgumentNullException.class, () -> ((ShortArrayEnumerable) Linq.of(new short[]{1, 2, 3, 2, 1}))._findLastIndex(null));
+
+        assertThrows(ArgumentNullException.class, () -> ((IntegerArrayEnumerable) Linq.of(new int[]{1, 2, 3, 2, 1}))._findLastIndex(null));
+        assertThrows(ArgumentNullException.class, () -> ((IntegerArrayEnumerable) Linq.of(new int[]{1, 2, 3, 2, 1}))._findLastIndex(null));
+
+        assertThrows(ArgumentNullException.class, () -> ((LongArrayEnumerable) Linq.of(new long[]{1L, 2L, 3L, 2L, 1L}))._findLastIndex(null));
+        assertThrows(ArgumentNullException.class, () -> ((LongArrayEnumerable) Linq.of(new long[]{1L, 2L, 3L, 2L, 1L}))._findLastIndex(null));
+
+        assertThrows(ArgumentNullException.class, () -> ((FloatArrayEnumerable) Linq.of(new float[]{1f, 2f, 3f, 2f, 1f}))._findLastIndex(null));
+        assertThrows(ArgumentNullException.class, () -> ((FloatArrayEnumerable) Linq.of(new float[]{1f, 2f, 3f, 2f, 1f}))._findLastIndex(null));
+
+        assertThrows(ArgumentNullException.class, () -> ((DoubleArrayEnumerable) Linq.of(new double[]{1d, 2d, 3d, 2d, 1d}))._findLastIndex(null));
+        assertThrows(ArgumentNullException.class, () -> ((DoubleArrayEnumerable) Linq.of(new double[]{1d, 2d, 3d, 2d, 1d}))._findLastIndex(null));
+
+        assertThrows(ArgumentNullException.class, () -> ((CharacterArrayEnumerable) Linq.of(new char[]{'1', '2', '3', '2', '1'}))._findLastIndex(null));
+        assertThrows(ArgumentNullException.class, () -> ((CharacterArrayEnumerable) Linq.of(new char[]{'1', '2', '3', '2', '1'}))._findLastIndex(null));
+
+        assertThrows(ArgumentNullException.class, () -> ((CharSequenceEnumerable) Linq.of("12321"))._findLastIndex(null));
+        assertThrows(ArgumentNullException.class, () -> ((CharSequenceEnumerable) Linq.of("12321"))._findLastIndex(null));
+
+        assertThrows(ArgumentNullException.class, () -> ((GenericArrayEnumerable<Integer>) Linq.of(1, 2, 3, 2, 1))._findLastIndex(null));
+        assertThrows(ArgumentNullException.class, () -> ((GenericArrayEnumerable<Integer>) Linq.of(1, 2, 3, 2, 1))._findLastIndex(null));
+
+        assertThrows(ArgumentNullException.class, () -> ((ArrayListEnumerable<Integer>) Linq.of(Arrays.asList(1, 2, 3, 2, 1)))._findLastIndex(null));
+        assertThrows(ArgumentNullException.class, () -> ((ArrayListEnumerable<Integer>) Linq.of(Arrays.asList(1, 2, 3, 2, 1)))._findLastIndex(null));
+
+        assertThrows(ArgumentNullException.class, () -> ((LinkedListEnumerable<Integer>) Linq.of(new LinkedList<>(Arrays.asList(1, 2, 3, 2, 1))))._findLastIndex(null));
+        assertThrows(ArgumentNullException.class, () -> ((LinkedListEnumerable<Integer>) Linq.of(new LinkedList<>(Arrays.asList(1, 2, 3, 2, 1))))._findLastIndex(null));
+
+        assertThrows(ArgumentNullException.class, () -> ((RunOnceLinkedList<Integer>) Linq.of(1, 2, 3, 2, 1).runOnce())._findLastIndex(null));
+        assertThrows(ArgumentNullException.class, () -> ((RunOnceLinkedList<Integer>) Linq.of(1, 2, 3, 2, 1).runOnce())._findLastIndex(null));
     }
 
     @Test
