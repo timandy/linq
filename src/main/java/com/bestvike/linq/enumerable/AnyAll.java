cc942928ee96be73841c9e74c8de6c94e90b0198
TimAndy <xuchonglei@126.com>
2019-08-20 09:57:46 +0800

    use _getCount() in any if available

--- src/main/java/com/bestvike/linq/enumerable/AnyAll.java
+++ src/main/java/com/bestvike/linq/enumerable/AnyAll.java
@@ -1,5 +1,6 @@
 package com.bestvike.linq.enumerable;
 
+import com.bestvike.collections.generic.ICollection;
 import com.bestvike.function.Predicate1;
 import com.bestvike.linq.IEnumerable;
 import com.bestvike.linq.IEnumerator;
@@ -16,6 +17,24 @@ public final class AnyAll {
     public static <TSource> boolean any(IEnumerable<TSource> source) {
         if (source == null)
             ThrowHelper.throwArgumentNullException(ExceptionArgument.source);
+
+        if (source instanceof ICollection) {
+            ICollection<TSource> collectionoft = (ICollection<TSource>) source;
+            return collectionoft._getCount() != 0;
+        }
+
+        if (source instanceof IIListProvider) {
+            // Note that this check differs from the corresponding check in
+            // Count (whereas otherwise this method parallels it).  If the count
+            // can't be retrieved cheaply, that likely means we'd need to iterate
+            // through the entire sequence in order to get the count, and in that
+            // case, we'll generally be better off falling through to the logic
+            // below that only enumerates at most a single element.
+            IIListProvider<TSource> listProv = (IIListProvider<TSource>) source;
+            int count = listProv._getCount(true);
+            if (count >= 0)
+                return count != 0;
+        }
 
         try (IEnumerator<TSource> e = source.enumerator()) {
             return e.moveNext();
