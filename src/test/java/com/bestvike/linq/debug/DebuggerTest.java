258b003a77c3716004da4f70f86d17a3b1c14107
TimAndy <xuchonglei@126.com>
2019-12-07 16:08:06 +0800

    modify debugger testcase

--- src/test/java/com/bestvike/linq/debug/DebuggerTest.java
+++ src/test/java/com/bestvike/linq/debug/DebuggerTest.java
@@ -15,12 +15,6 @@ class DebuggerTest extends TestCase {
         LacksBrace lacksBrace = new LacksBrace();
         assertThrows(InvalidOperationException.class, () -> Debugger.getDebuggerDisplay(lacksBrace));
 
-        ErrorExpression errorExpression = new ErrorExpression();
-        assertThrows(InvalidOperationException.class, () -> Debugger.getDebuggerDisplay(errorExpression));
-    }
-
-    @Test
-    void testMethod() {
         StaticMethod staticMethod = new StaticMethod();
         assertEquals("value=\"hello\"", Debugger.getDebuggerDisplay(staticMethod));
 
@@ -32,10 +26,7 @@ class DebuggerTest extends TestCase {
 
         NoMethod noMethod = new NoMethod();
         assertThrows(InvalidOperationException.class, () -> Debugger.getDebuggerDisplay(noMethod));
-    }
 
-    @Test
-    void testField() {
         StaticField staticField = new StaticField();
         assertEquals("value=\"hello\"", Debugger.getDebuggerDisplay(staticField));
 
@@ -44,25 +35,24 @@ class DebuggerTest extends TestCase {
 
         NoField noField = new NoField();
         assertThrows(InvalidOperationException.class, () -> Debugger.getDebuggerDisplay(noField));
+
+        Apple apple = new Apple("fuji", "red");
+        assertEquals("Style = fuji, Color = \"red\"", Debugger.getDebuggerDisplay(apple));
     }
 
     @Test
-    void testDebugView() {
+    void testGetDebugView() {
+        Pen pen = new Pen();
+        assertThrows(InvalidOperationException.class, () -> Debugger.getDebugView(pen));
+
         People people = new People();
         IDebugView peopleDebugView = Debugger.getDebugView(people);
         assertIsType(PeopleDebugView.class, peopleDebugView);
-
-        Apple apple = new Apple();
-        assertThrows(InvalidOperationException.class, () -> Debugger.getDebugView(apple));
     }
 
 
     @DebuggerDisplay("hello {world")
     private static class LacksBrace {
-    }
-
-    @DebuggerDisplay("value={value}")
-    private static class ErrorExpression {
     }
 
     @DebuggerDisplay("value={value()}")
@@ -108,6 +98,46 @@ class DebuggerTest extends TestCase {
         private String value = "hello";
     }
 
+    @DebuggerDisplay("Style = {getStyle(),nq}, Color = {color}")
+    private static class Apple {
+        private final String style;
+        private final String color;
+
+        private Apple(String style, String color) {
+            this.style = style;
+            this.color = color;
+        }
+
+        public String getStyle() {
+            return this.style;
+        }
+
+        public String getColor() {
+            return this.color;
+        }
+    }
+
+    private static class PenDebugView implements IDebugView {
+        private final Pen pen;
+
+        private PenDebugView() {
+            this.pen = null;
+        }
+
+        private PenDebugView(Object apple) {
+            throw new RuntimeException();
+        }
+
+        @Override
+        public Object getProxyObject() {
+            return this.pen;
+        }
+    }
+
+    @DebuggerTypeProxy(PenDebugView.class)
+    private static class Pen {
+    }
+
     private static class PeopleDebugView implements IDebugView {
         private final People people;
 
@@ -131,26 +161,5 @@ class DebuggerTest extends TestCase {
 
     @DebuggerTypeProxy(PeopleDebugView.class)
     private static class People {
-    }
-
-    private static class AppleDebugView implements IDebugView {
-        private final Apple apple;
-
-        private AppleDebugView() {
-            this.apple = null;
-        }
-
-        private AppleDebugView(Object apple) {
-            throw new RuntimeException();
-        }
-
-        @Override
-        public Object getProxyObject() {
-            return this.apple;
-        }
-    }
-
-    @DebuggerTypeProxy(AppleDebugView.class)
-    private static class Apple {
     }
 }
