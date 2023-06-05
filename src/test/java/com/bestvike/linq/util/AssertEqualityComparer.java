9528afc29f033120e9fa5590c6dcc130f0a64cff
TimAndy <xuchonglei@126.com>
2023-06-05 14:48:59 +0800

    TestBase support array equals on sequenceEqual

--- src/test/java/com/bestvike/linq/util/AssertEqualityComparer.java
+++ src/test/java/com/bestvike/linq/util/AssertEqualityComparer.java
@@ -2,6 +2,7 @@ package com.bestvike.linq.util;
 
 import com.bestvike.collections.generic.IEqualityComparer;
 import com.bestvike.linq.IEnumerable;
+import com.bestvike.linq.Linq;
 
 import java.util.Objects;
 
@@ -13,8 +14,21 @@ public final class AssertEqualityComparer<T> implements IEqualityComparer<T> {
 
     @Override
     public boolean equals(T x, T y) {
-        if (x instanceof IEnumerable && y instanceof IEnumerable)
-            return ((IEnumerable) x).sequenceEqual((IEnumerable) y, innerComparer);
+        IEnumerable left = null;
+        IEnumerable right = null;
+
+        if (x instanceof Object[])
+            left = Linq.of((Object[]) x);
+        else if (x instanceof IEnumerable)
+            left = ((IEnumerable<?>) x);
+
+        if (y instanceof Object[])
+            right = Linq.of((Object[]) y);
+        else if (y instanceof IEnumerable)
+            right = ((IEnumerable<?>) y);
+
+        if (left != null && right != null)
+            return left.sequenceEqual(right, innerComparer);
         return Objects.equals(x, y);
     }
 
