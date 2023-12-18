9b4c319aed2c2558cfccd707107b1f15b04f3e7e
TimAndy <xuchonglei@126.com>
2023-12-18 13:56:36 +0800

    Add float and double comparer

--- src/main/java/com/bestvike/collections/generic/Comparer.java
+++ src/main/java/com/bestvike/collections/generic/Comparer.java
@@ -14,6 +14,8 @@ import java.util.Comparator;
 public final class Comparer<T> implements Comparator<T> {
     private static final Comparer<?> DEFAULT = new Comparer<>(null);
     private static final Comparer<?> DEFAULT_INVARIANT = new Comparer<>(CultureInfo.getInvariantCulture());
+    private static final Comparator<Float> FLOAT = new FloatComparer();
+    private static final Comparator<Double> DOUBLE = new DoubleComparer();
 
     private final Collator collator;
 
@@ -29,6 +31,14 @@ public final class Comparer<T> implements Comparator<T> {
     public static <T> Comparator<T> DefaultInvariant() {
         //noinspection unchecked
         return (Comparator<T>) DEFAULT_INVARIANT;
+    }
+
+    public static Comparator<Float> Float() {
+        return FLOAT;
+    }
+
+    public static Comparator<Double> Double() {
+        return DOUBLE;
     }
 
     public static <T> Comparator<T> create(Collator collator) {
@@ -61,5 +71,65 @@ public final class Comparer<T> implements Comparator<T> {
             return -((Comparable<T>) y).compareTo(x);
         ThrowHelper.throwImplementComparableException();
         return 0;
+    }
+
+
+    private static final class FloatComparer implements Comparator<Float> {
+        @Override
+        public int compare(Float x, Float y) {
+            if (x != null) {
+                if (y != null)
+                    return this.comparePrimitive(x, y);
+                return 1;
+            }
+            if (y != null)
+                return -1;
+            return 0;
+        }
+
+        private int comparePrimitive(float x, float y) {
+            if (x < y)
+                return -1;
+            if (x > y)
+                return 1;
+            if (x == y)
+                return 0;
+
+            // At least one of the values is NaN.
+            if (Float.isNaN(x))
+                return Float.isNaN(y) ? 0 : -1;
+            else // y is NaN.
+                return 1;
+        }
+    }
+
+
+    private static final class DoubleComparer implements Comparator<Double> {
+        @Override
+        public int compare(Double x, Double y) {
+            if (x != null) {
+                if (y != null)
+                    return this.comparePrimitive(x, y);
+                return 1;
+            }
+            if (y != null)
+                return -1;
+            return 0;
+        }
+
+        private int comparePrimitive(double x, double y) {
+            if (x < y)
+                return -1;
+            if (x > y)
+                return 1;
+            if (x == y)
+                return 0;
+
+            // At least one of the values is NaN.
+            if (Double.isNaN(x))
+                return Double.isNaN(y) ? 0 : -1;
+            else // y is NaN.
+                return 1;
+        }
     }
 }
